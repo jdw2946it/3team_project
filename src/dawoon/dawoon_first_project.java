c24ce80fff9dac5f56f95f20b947a0bee0e6742d
user <user@NucBox_M6>
2025-08-27 10:48:50 +0900

    정다운 프로젝트

--- src/dawoon/dawoon_first_project.java
+++ src/dawoon/dawoon_first_project.java
@@ -3,7 +3,7 @@ package dawoon;
 public class dawoon_first_project {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World_dawoon");
+		System.out.println("Hello World_dawoonnnnnnn");
 	}
 
 }
