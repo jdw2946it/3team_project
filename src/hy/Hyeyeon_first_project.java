f8fcf2a3d4c16c272caf23ed1b6fcace67f9f6cc
user <user@NucBox_M6>
2025-08-27 10:32:38 +0900

    h

--- src/hy/Hyeyeon_first_project.java
+++ src/hy/Hyeyeon_first_project.java
@@ -2,7 +2,7 @@ package hy;
 
 public class Hyeyeon_first_project {
 	public static void main(String[] args) {
-		System.out.println("Hello World_hhh");
+		System.out.println("Hello World_hh");
 	}
 
 }
